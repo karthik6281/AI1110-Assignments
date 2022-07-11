317e2c8a56f02ec33e08d5e37d06f078f6da0b75
karthik6281 <102568642+karthik6281@users.noreply.github.com>
2022-07-11 11:24:12 +0530

    Add files via upload

---
+++ random_numbers/6/6_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main(void) //main function begins
+{
+ 
+chisquare("chi.dat",2,1000000);
+
+//Mean of uniform
+//printf("%lf",mean("uni.dat"));
+return 0;
+}
+
+
